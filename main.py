e744504f3360b7f99961f5084af27c390aa92132
neog4f1 <37392219+neog4f1@users.noreply.github.com>
2022-12-05 19:16:48 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3,10 +3,30 @@ import sublime_plugin
 # import json
 import re
 
+class Settings:
+  settings = sublime.load_settings('tailwind_reorder.sublime-settings')
+
+  @staticmethod
+  def init():
+    Settings.settings.add_on_change('tailwind_reorder' + '-reload', Settings.setup)
+    Settings.setup()
+
+  @staticmethod
+  def setup():
+    Settings.filter_by = Settings.settings.get('filter_by', 0)
+    Settings.classNames = Settings.settings.get('classNames', 0)
+    Settings.scopes = Settings.settings.get('scopes', 0)
+    Settings.data = Settings.settings.get('data', 0)
+
+def plugin_loaded():
+    Settings.init()
+    print(Settings.scopes)
+
 class TailwindOrderCommand(sublime_plugin.TextCommand):
 
     def getRegexClassNames(self):
-        classNames = self.settings.get('classNames')
+        classNames = Settings.classNames
+        # classNames = self.settings.get('classNames')
         regex = "(?:"
         for item in classNames:
             regex += '(?<=' + item + '=")|'
@@ -14,15 +34,16 @@ class TailwindOrderCommand(sublime_plugin.TextCommand):
         # '(?:(?<=class=")|(?<=className="))(.*?)(?=")'
         return regex
 
-    def checkScope1(self):
-        scopes = self.settings.get('scopes')
-        cursor = self.view.sel()[0].begin()
-        curr_scope = view.scope_name(cursor)
+    # def checkScope1(self):
+    #     # scopes = self.settings.get('scopes')
+    #     cursor = self.view.sel()[0].begin()
+    #     curr_scope = view.scope_name(cursor)
 
-        return (curr_scope in scopes)
+    #     return (curr_scope in scopes)
 
     def checkScope(self):
-        scopes = self.settings.get('scopes')
+        scopes = Settings.scopes
+        # scopes = self.settings.get('scopes')
         # matchHTMLString = view.match_selector(locations[0], "text.html string.quoted")
         match = next(filter(lambda scope: self.view.find_by_selector(scope), scopes), None)
         return match
@@ -34,13 +55,15 @@ class TailwindOrderCommand(sublime_plugin.TextCommand):
         return filter_by
 
     def run(self, edit):
-        if not hasattr(self, "settings"):
-            self.settings = sublime.load_settings("tailwind-order.sublime-settings")
+        # if not hasattr(self, "settings"):
+        #     self.settings = sublime.load_settings("tailwind-order.sublime-settings")
         if not self.checkScope():
             return 0
         regex = self.getRegexClassNames()
-        list = self.settings.get('filter_by')
-        file = self.settings.get('data')
+        filter_by = Settings.filter_by
+        # filter_by = self.settings.get('filter_by')
+        file = Settings.data
+        # file = self.settings.get('data')
 
         # file = sublime.load_resource(sublime.find_resources('data.json')[0])
         # file = json.loads(file)
@@ -65,7 +88,7 @@ class TailwindOrderCommand(sublime_plugin.TextCommand):
                     self.view.replace(edit, item, temp_str)
                     dif += len(temp_str) - len(originStr)
                 continue
-            filters = self.create_filters(list)
+            filters = self.create_filters(filter_by)
 
             other_classes = temp_classes[:]
             sorted_class = ""
@@ -78,7 +101,7 @@ class TailwindOrderCommand(sublime_plugin.TextCommand):
                             if temp_class in other_classes:
                                 other_classes.remove(temp_class)
                         break # because flex-wrap will have flex and flex-
-            for kind in list:
+            for kind in filter_by:
             # for kind in filters.keys():
                 if not filters[kind]:
                     continue
